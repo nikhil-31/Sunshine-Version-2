860cf462dfbec800a5ba3c59b8460edab7702526
nkl07ba <nkl07ba@gmail.com>
2016-09-24 12:07:01 +0530

    Added text that shows when there is no data in the database
    This is part of the error handling, i am using this to indicate if there is no weather data in the database and no internet connectivity to update the database

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -139,6 +139,8 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
         // Get a reference to the ListView, and attach this adapter to it.
         mListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        View setEmptyView = rootView.findViewById(R.id.empty);
+        mListView.setEmptyView(setEmptyView);
         mListView.setAdapter(mForecastAdapter);
         // We'll call our MainActivity
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
